96071b037de34e63e2de72962b6dbe76ba08e0fc
Manuel <github@manuelbieh.de>
2016-12-21 00:23:53 +0100

    get IP example

--- client/Components/App.js
+++ client/Components/App.js
@@ -12,15 +12,46 @@ const AnotherRoute = () => {
 const Layout = ({ children }) => {
     return (
         <div>
-            <Link to="/counter-1">Counter 1</Link> | <Link to="/counter-2">Counter 2</Link>
+            <Link to="/counter-1">Counter 1</Link> | <Link to="/counter-2">Counter 2</Link> | <Link to="/ip">IP</Link>
             { children }
         </div>
     );
 };
 
 Layout.propTypes = {
-    children: React.PropTypes.number.isRequired
+    children: React.PropTypes.node
 };
+
+
+class ShowMyIP extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            ip: '???'
+        }
+    }
+    
+    componentDidMount() {
+        fetch('https://wtfismyip.com/json').then((response) => {
+            response.json().then((data) => {
+                this.setState({
+                    ip: data.YourFuckingIPAddress
+                });
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                { this.state.ip }
+            </div>
+        )
+    }
+
+}
+
 
 export default function App() {
     return (
@@ -28,6 +59,7 @@ export default function App() {
             <Route path="/" component={ Layout }>
                 <Route path="counter-1" component={ Counter } />
                 <Route path="counter-2" component={ AnotherRoute } />
+                <Route path="ip" component={ ShowMyIP } />
             </Route>
         </Router>
     );
