152ea6463fef69c5dac2e9754525c43ead36da01
Manuel Bieh <info@manuelbieh.de>
2018-03-17 21:51:36 +0100

    added some more usefull postcss-plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,12 @@
 module.exports = {
     plugins: [
+        require('postcss-flexbugs-fixes')(),
+        require('postcss-nested')(),
+        require('postcss-custom-properties')(),
         require('autoprefixer')({
             browsers: ['last 3 versions', 'ie >= 9', 'chrome 4', 'Edge <= 15'],
         }),
+        require('postcss-custom-properties')(),
         require('postcss-assets')({
             basePath: './assets',
         }),
