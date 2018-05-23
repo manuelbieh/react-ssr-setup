63d66dc6c4517e8e9e2abdab3b549c19cc9d7772
Manuel Bieh <info@manuelbieh.de>
2018-05-23 13:08:30 +0200

    added client-side opt-in

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,40 @@
 const webpack = require('webpack');
 const rimraf = require('rimraf');
+
 const webpackConfig = require('../config/webpack.config.js')(process.env.NODE_ENV || 'production');
 const paths = require('../config/paths');
 const { logMessage, compilerPromise } = require('./utils');
+
+const generateStaticHTML = async () => {
+    const nodemon = require('nodemon');
+    const fs = require('fs');
+    const puppeteer = require('puppeteer');
+
+    process.env.PORT = 8505;
+
+    const script = nodemon({
+        script: `${paths.serverBuild}/server.js`,
+        ignore: ['*'],
+    });
+
+    script.on('start', async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(`http://localhost:${process.env.PORT}`);
+        const pageContent = await page.content();
+        fs.writeFileSync(`${paths.clientBuild}/index.html`, pageContent);
+        await browser.close();
+        process.exit();
+    });
+
+    script.on('quit', () => {
+        process.exit();
+    });
+
+    script.on('error', () => {
+        process.exit(1);
+    });
+};
 
 const build = async () => {
     rimraf.sync(paths.clientBuild);
@@ -35,8 +67,8 @@ const build = async () => {
     try {
         await serverPromise;
         await clientPromise;
+        await generateStaticHTML();
         logMessage('Done!', 'info');
-        process.exit();
     } catch (error) {
         logMessage(error, 'error');
     }
--- src/server/render.js
+++ src/server/render.js
@@ -20,15 +20,16 @@ const serverRenderer = () => (req, res) => {
     const state = JSON.stringify(req.store.getState());
 
     return res.send(
-        renderToString(
-            <Html
-                css={[res.locals.assetPath('bundle.css'), res.locals.assetPath('vendor.css')]}
-                scripts={[res.locals.assetPath('bundle.js'), res.locals.assetPath('vendor.js')]}
-                state={state}
-            >
-                {content}
-            </Html>
-        )
+        '<!doctype html>' +
+            renderToString(
+                <Html
+                    css={[res.locals.assetPath('bundle.css'), res.locals.assetPath('vendor.css')]}
+                    scripts={[res.locals.assetPath('bundle.js'), res.locals.assetPath('vendor.js')]}
+                    state={state}
+                >
+                    {content}
+                </Html>
+            )
     );
 };
 
