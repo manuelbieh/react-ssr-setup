d90d5b84ce1ff086770cb54354f1485d49bd6797
Manuel Bieh <info@manuelbieh.de>
2018-12-20 13:41:09 +0100

    experimentally updated to jest 24 alpha

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
         '<rootDir>/node_modules/regenerator-runtime/runtime',
         '<rootDir>/config/polyfills.js',
     ],
-    setupTestFrameworkScriptFile: '<rootDir>config/jest/setup.js',
+    setupFilesAfterEnv: ['<rootDir>config/jest/setup.js'],
     testMatch: [
         '<rootDir>/src/**/__tests__/**/*.{js,jsx,mjs}',
         '<rootDir>/src/**/?(*.)(spec|test).{js,jsx,mjs}',
