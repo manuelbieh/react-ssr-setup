947e42449dacdc29900f2dbc375343b007f20c9a
Manuel Bieh <info@manuelbieh.de>
2019-01-23 01:08:27 +0100

    failed attempt to streaming server rendering with ReactDOMFizzServer

--- src/server/render.js
+++ src/server/render.js
@@ -1,36 +1,69 @@
 import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { StaticRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import IntlProvider from '../shared/i18n/IntlProvider';
-import Html from './components/HTML';
-import App from '../shared/App';
+import Stream from 'stream';
+import { pipeToNodeWritable } from 'react-dom/unstable-fizz';
+// import { renderToString } from 'react-dom/server';
+// import { StaticRouter as Router } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import IntlProvider from '../shared/i18n/IntlProvider';
+// import Html from './components/HTML';
+// import App from '../shared/App';
+
+// const Suspense = React.Suspense;
+// const Suspense = 'div';
+
+const getWritableStream = () => {
+    const writable = new Stream.PassThrough();
+    writable.setEncoding('utf8');
+    writable.result = '';
+    writable.on('data', (chunk) => (writable.result += chunk));
+    return writable;
+};
 
 const serverRenderer = () => (req, res) => {
-    const content = renderToString(
-        <Provider store={req.store}>
-            <Router location={req.url} context={{}}>
-                <IntlProvider>
-                    <App />
-                </IntlProvider>
-            </Router>
-        </Provider>
+    const stream = getWritableStream();
+
+    res.write('<html>');
+    pipeToNodeWritable(
+        <div style={{ color: 'red' }}>
+            LOL!
+            {/* <Suspense fallback={<div>Fallback</div>}>
+                <div>nested div that breaks everything</div>
+            </Suspense> */}
+        </div>,
+        stream
     );
 
-    const state = JSON.stringify(req.store.getState());
-
-    return res.send(
-        '<!doctype html>' +
-            renderToString(
-                <Html
-                    css={[res.locals.assetPath('bundle.css'), res.locals.assetPath('vendor.css')]}
-                    scripts={[res.locals.assetPath('bundle.js'), res.locals.assetPath('vendor.js')]}
-                    state={state}
-                >
-                    {content}
-                </Html>
-            )
-    );
+    stream.pipe(res);
+    stream.on('finish', () => {
+        res.end('</html>');
+    });
 };
+
+// const serverRenderer = () => (req, res) => {
+//     const content = renderToString(
+//         <Provider store={req.store}>
+//             <Router location={req.url} context={{}}>
+//                 <IntlProvider>
+//                     <App />
+//                 </IntlProvider>
+//             </Router>
+//         </Provider>
+//     );
+//
+//     const state = JSON.stringify(req.store.getState());
+//
+//     return res.send(
+//         '<!doctype html>' +
+//             renderToString(
+//                 <Html
+//                     css={[res.locals.assetPath('bundle.css'), res.locals.assetPath('vendor.css')]}
+//                     scripts={[res.locals.assetPath('bundle.js'), res.locals.assetPath('vendor.js')]}
+//                     state={state}
+//                 >
+//                     {content}
+//                 </Html>
+//             )
+//     );
+// };
 
 export default serverRenderer;
--- src/shared/App.js
+++ src/shared/App.js
@@ -8,10 +8,15 @@ import { ReactComponent as ReactLogo } from './assets/react.svg';
 import Features from './components/Features';
 import css from './App.module.css';
 
+// const Page1 = React.lazy(() => import('pages/Page-1'));
+
 type PropsT = {
     setLocale: (string) => {},
     t: (string) => string,
 };
+
+// const { Suspense } = React;
+const Suspense = 'div';
 
 class App extends React.PureComponent<PropsT> {
     setLanguage = (e: SyntheticInputEvent<HTMLButtonElement>) => {
@@ -22,25 +27,30 @@ class App extends React.PureComponent<PropsT> {
         const { t } = this.props;
 
         return (
-            <div className={css.wrapper}>
-                <Helmet defaultTitle="React SSR Starter" titleTemplate="%s – React SSR Starter" />
+            <Suspense fallback={<div>Loading</div>}>
+                <div className={css.wrapper}>
+                    <Helmet
+                        defaultTitle="React SSR Starter"
+                        titleTemplate="%s – React SSR Starter"
+                    />
 
-                <h1>
-                    <ReactLogo className={css.reactLogo} /> React + Express – SSR Starter
-                </h1>
+                    <h1>
+                        <ReactLogo className={css.reactLogo} /> React + Express – SSR Starter
+                    </h1>
 
-                <Features />
+                    <Features />
 
-                <h2>{t('i18n-example')}</h2>
-                <p>
-                    <button value="de-DE" onClick={this.setLanguage}>
-                        Deutsch
-                    </button>
-                    <button value="en-US" onClick={this.setLanguage}>
-                        English
-                    </button>
-                </p>
-            </div>
+                    <h2>{t('i18n-example')}</h2>
+                    <p>
+                        <button value="de-DE" onClick={this.setLanguage}>
+                            Deutsch
+                        </button>
+                        <button value="en-US" onClick={this.setLanguage}>
+                            English
+                        </button>
+                    </p>
+                </div>
+            </Suspense>
         );
     }
 }
