435d24be46a9ebb0192c33c18dac82b8a30b926e
Manuel Bieh <info@manuelbieh.de>
2019-02-07 16:07:24 +0100

    preparing --client-only dev/build process, added babel-plugin-macros

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,7 +1,14 @@
 const chalk = require('chalk');
 
 const logMessage = (message, level = 'info') => {
-    const color = level === 'error' ? 'red' : level === 'warning' ? 'yellow' : 'white';
+    const color =
+        level === 'error'
+            ? 'red'
+            : level === 'warning'
+            ? 'yellow'
+            : level === 'info'
+            ? 'blue'
+            : 'white';
     console.log(`[${new Date().toISOString()}]`, chalk[color](message));
 };
 
