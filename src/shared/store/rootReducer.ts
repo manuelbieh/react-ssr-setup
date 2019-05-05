e132ac948ab97c38395998bf72acf4a7adc7e57c
Manuel Bieh <info@manuelbieh.de>
2019-05-05 17:46:00 +0200

    using correct types and parameters for history calls (#61)

--- src/shared/store/rootReducer.ts
+++ src/shared/store/rootReducer.ts
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
+import { History } from 'history';
 import app from './app/reducer';
 
-const createRootReducer = (history) =>
+const createRootReducer = (history: History) =>
     combineReducers({
         app,
         router: connectRouter(history),
