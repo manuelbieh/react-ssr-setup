14e0e451203ad84d7a7d29b5812b1b4584cd6bb1
Manuel Bieh <info@manuelbieh.de>
2019-07-24 19:05:29 +0200

    fixed typedef

--- src/shared/i18n/IntlProvider.tsx
+++ src/shared/i18n/IntlProvider.tsx
@@ -42,7 +42,7 @@ const mapStateToProps = (state: any) => ({
     locale: getLocale(state),
 });
 
-export default withRouter<any>(
+export default withRouter<any, any>(
     connect(
         mapStateToProps,
         null,
