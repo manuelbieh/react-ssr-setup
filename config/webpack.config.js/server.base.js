6d440a0716e950b8bb05f381c664771be756871d
Manuel Bieh <info@manuelbieh.de>
2019-08-02 11:25:43 +0200

    improve build output

--- config/webpack.config.js/server.base.js
+++ config/webpack.config.js/server.base.js
@@ -36,6 +36,18 @@ module.exports = {
     },
     plugins: [...plugins.shared, ...plugins.server],
     stats: {
+        assets: false,
+        cached: false,
+        cachedAssets: false,
+        chunks: false,
+        chunkModules: false,
+        children: false,
         colors: true,
+        hash: false,
+        modules: false,
+        performance: false,
+        reasons: false,
+        timings: true,
+        version: false,
     },
 };
