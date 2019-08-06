a56bfff1c80ba3424be522d19103dea40807c2c5
Manuel Bieh <info@manuelbieh.de>
2019-08-06 18:18:33 +0200

    add resolve alias for react and react-dom to avoid potential issues

--- config/webpack.config.js/resolvers.js
+++ config/webpack.config.js/resolvers.js
@@ -3,4 +3,11 @@ const paths = require('../paths');
 module.exports = {
     extensions: ['.js', '.mjs', '.json', '.jsx', '.ts', '.tsx', '.css'],
     modules: paths.resolveModules,
+    alias: {
+        /* When working with linked modules which have their own node_modules we must make sure
+        to always load the same React version in all components. So we define an alias here and
+        resolve it to node_modules/* to avoid potential issues */
+        'react': require.resolve('react'),
+        'react-dom': require.resolve('react-dom'),
+    },
 };
