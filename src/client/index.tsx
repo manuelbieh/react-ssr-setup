56528ab51e8a577df8070ba151c677adfb539446
Manuel Bieh <info@manuelbieh.de>
2020-04-18 15:30:36 +0200

    Rewrite import style to avoid possibly bigger bundle size (see https://twitter.com/sebmarkbage/status/1250284377138802689)

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { hydrate } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
--- src/shared/App.tsx
+++ src/shared/App.tsx
@@ -1,5 +1,5 @@
 // import React, { Suspense } from 'react';
-import React from 'react';
+import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
 import { Link, Route, Switch } from 'react-router-dom';
