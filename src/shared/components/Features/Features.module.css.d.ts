37b65b91d9f66bae203ee77e80841fe20c6e2d2b
Manuel Bieh <github@manuelbieh.de>
2020-05-11 23:08:50 +0100

    chore: add type defs for css modules

---
+++ src/shared/components/Features/Features.module.css.d.ts
@@ -0,0 +1,12 @@
+declare const styles: {
+    readonly hot: string;
+    readonly i18n: string;
+    readonly jest: string;
+    readonly linter: string;
+    readonly racecar: string;
+    readonly react: string;
+    readonly rtl: string;
+    readonly webpack: string;
+    readonly wrapper: string;
+};
+export = styles;
---
+++ src/shared/pages/Page-1/Page-1.module.css.d.ts
@@ -0,0 +1,4 @@
+declare const styles: {
+    readonly wrapper: string;
+};
+export = styles;
