3f4bf31cdbc353bd9944b7201b2da11f83b4e809
Manuel Bieh <github@manuelbieh.de>
2022-02-11 11:37:20 +0100

    Update i18n-push.ts

--- scripts/i18n-push.ts
+++ scripts/i18n-push.ts
@@ -4,12 +4,6 @@ import glob from 'glob';
 import axios from 'axios';
 import paths from '../config/paths';
 
-// curl --request POST \
-//   --url https://api.lokalise.com/api2/projects/562546565d15501e66e558.60519915/files/upload \
-//   --header 'content-type: application/json' \
-//   --header 'x-api-token: aea09c4a67208079b4c51d9e75ccf62beb247581' \
-//   --data '{"filename":"api-import.json","data":"ewogICJmZWF0dXJlcyI6ICJGZWF0dXJlcyIsCiAgImkxOG4tZXhhbXBsZSI6ICJpMThuIEV4YW1wbGUiLAogICJpMThuLXN1cHBvcnQiOiAiaTE4biBzdXBwb3J0ICh2aWEgUmVhY3QgaTE4TmV4dCkiCn0K","lang_iso":"en","tags":["index","admin","v2.0"],"convert_placeholders":true}'
-
 const upload = async () => {
     const translationFiles = glob.sync(
         path.join(paths.locales, process.env.SOURCE_LANGUAGE, '**/*.json')
